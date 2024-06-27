c5c687a1ef2966b230ad840c45cc474a7570bcec
andre <andreadn0312@gmail.com>
2024-06-27 19:06:20 -0300

    feature/footer

---
+++ src/views/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import './Footer.sass';
+
+import logo2 from '../../../images/logo2.png';
+
+const Footer = () => {
+
+    return (
+        <>
+            <footer>
+                <div className="container">
+                    <div className="footer-up">
+                        <div className="footer-left">
+                            <Link className="item" to="/">
+                                Home
+                            </Link>
+
+                            <Link className="item" to="#doguinhos">
+                                Doguinhos
+                            </Link>
+
+                            {/* <div className="item">Sobre</div>
+                            <div className="item">Contato</div> */}
+
+                            <div className="footer-right"></div>
+                        </div>
+                    </div>
+                    <div className="footer-down">
+                        <div className="copy">@ 2022 Adote-me. Todos os direitos reservados</div>
+                        <div className="logo">
+                            <img src={logo2} width="115" alt="" srcset="" />
+                        </div>
+                        {/* <div className="links">
+                            <div className="item">Política de privacidade</div>
+                            <div className="item">Política de privacidade</div>
+                        </div> */}
+                    </div>
+
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
