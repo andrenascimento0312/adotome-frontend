90110e7660fc50eb147acc5636b310d08188e61c
andre <andreadn0312@gmail.com>
2024-06-27 19:25:31 -0300

    bugfix/redirect-on-first-render

--- src/views/components/ListingDogFilter/ListingDogFilter.jsx
+++ src/views/components/ListingDogFilter/ListingDogFilter.jsx
@@ -12,6 +12,7 @@ const ListingDogFilter = ({ dogs, pagination, setPagination, loading, listingRef
 
     const { currentPage, totalPosts, totalPages } = pagination;
     const location = useLocation();
+    const firstRender = useRef(true);
 
     useEffect(() => {
         if (location.hash !== '#doguinhos') return;
@@ -24,75 +25,74 @@ const ListingDogFilter = ({ dogs, pagination, setPagination, loading, listingRef
         }, 300);
     }, [location]);
 
-
     useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false; // Marca que não é mais a primeira renderização
+            return; 
+        }
+
         const dogsContainer = document.querySelector('.leftGroup');
         if (!dogsContainer) return;
+        
         setTimeout(() => {
             dogsContainer.scrollIntoView({ behavior: 'smooth' });
         }, 300);
-    }, [pagination])
+    }, [pagination, filters])
+    
 
-    useEffect(() => {
-        const dogsContainer = document.querySelector('.leftGroup');
-        if (!dogsContainer) return;
-        setTimeout(() => {
-            dogsContainer.scrollIntoView({ behavior: 'smooth' });
-        }, 300);
-    }, [filters])
 
-    return (
-        <>
-            <div className="container" ref={listingRef}>
+return (
+    <>
+        <div className="container" ref={listingRef}>
 
-                <div className={`listingDogFilter doguinhos`}>
+            <div className={`listingDogFilter doguinhos`}>
 
-                    {/* <MobileFilter
+                {/* <MobileFilter
                        
                     /> */}
 
-                    <Filter
-                        filters={filters}
-                        setFilters={setFilters}
-                    />
+                <Filter
+                    filters={filters}
+                    setFilters={setFilters}
+                />
 
-                    <div className="leftGroup">
-                        {/* tem que mostrar total posts */}
-                        <h5>Resultado: {totalPosts}</h5>
-                        <div className="dogsGroup">
-                            {dogs.map((dog, index) => (
-                                <DogCard
-                                    className={loading ? 'dogLoading' : ''}
-                                    key={index}
-                                    dogMainPhoto={dog.dogMainPhoto}
-                                    dogName={dog.dogName}
-                                    dogAge={dog.dogAge}
-                                    dogSize={dog.dogSize}
-                                    dogGender={dog.dogGender}
-                                    dogSlug={dog.dogSlug}
-                                />
-                            ))}
-                        </div>
-                        {/* preciso criar um estado redux na home e ler aqui */}
-                        <Pagination
-                            className="paginationItems"
-                            prev
-                            next
-                            maxButtons={5}
-                            size="lg"
-                            total={totalPosts} // Total de posts
-                            limit={6} // Número de itens por página
-                            activePage={currentPage}
-                            onChangePage={(newPage) => setPagination(prev => ({ ...prev, currentPage: newPage }))}
-                        />
+                <div className="leftGroup">
+                    {/* tem que mostrar total posts */}
+                    <h5>Resultado: {totalPosts}</h5>
+                    <div className="dogsGroup">
+                        {dogs.map((dog, index) => (
+                            <DogCard
+                                className={loading ? 'dogLoading' : ''}
+                                key={index}
+                                dogMainPhoto={dog.dogMainPhoto}
+                                dogName={dog.dogName}
+                                dogAge={dog.dogAge}
+                                dogSize={dog.dogSize}
+                                dogGender={dog.dogGender}
+                                dogSlug={dog.dogSlug}
+                            />
+                        ))}
                     </div>
+                    {/* preciso criar um estado redux na home e ler aqui */}
+                    <Pagination
+                        className="paginationItems"
+                        prev
+                        next
+                        maxButtons={5}
+                        size="lg"
+                        total={totalPosts} // Total de posts
+                        limit={6} // Número de itens por página
+                        activePage={currentPage}
+                        onChangePage={(newPage) => setPagination(prev => ({ ...prev, currentPage: newPage }))}
+                    />
                 </div>
-
-
             </div>
 
-        </>
-    )
+
+        </div>
+
+    </>
+)
 }
 
 export default ListingDogFilter;
