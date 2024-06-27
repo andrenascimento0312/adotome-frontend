da83d4e1cc72852c554659f215d1e45c99dd84fc
andre <andreadn0312@gmail.com>
2024-06-27 19:07:07 -0300

    feature/mobile-filter

---
+++ src/views/components/MobileFilter/MobileFilter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./MobileFilter.sass";
+
+const MobileFilter = () => {
+
+    return (
+        <>
+            <div className="mobile-filter-container">
+                <div className="item">Filtros</div>
+                <div className="item">clique</div>
+            </div>
+
+        </>
+    )
+}
+
+export default MobileFilter;
